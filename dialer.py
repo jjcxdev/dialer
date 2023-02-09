d59ae538f99dbe8a9b7bef8bf52eae804fd6fd13
Justin Chambers <112969641+justinjchambers@users.noreply.github.com>
2023-02-09 09:11:31 -0500

    Update dialer.py

--- dialer.py
+++ dialer.py
@@ -3,19 +3,23 @@ import csv
 from twilio.rest import Client
 
 # Set up Twilio client
-account_sid = "AC26575c358683591230883662ece06cb0"
-auth_token = "7ac2df20159aaa73b13b5676a6334711"
+account_sid = ""
+auth_token = ""
 client = Client(account_sid, auth_token)
 
-phone_number = "14164558684"  # Phone number to call
-interval = 30  # Call interval in seconds
+phone_number = ""  # Phone number to call
+my_number = ""
+interval = 5  # Call interval in seconds
+delay = 30  # Delay before TwiML is executed in seconds
 
 while True:
+
     # Place call
     call = client.calls.create(
         to=phone_number,
-        from_="+16693382345",
-        url="http://example.com/recorded_message.mp3"
+        from_=my_number,
+        # url="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+        twiml=f'<Response><Say>The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot. The shoe is on the other foot.</Say></Response>',
     )
     print(f"Calling {phone_number}...")
 
